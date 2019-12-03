85b031de019f10459cf08f25e13200249bcab6dd
Art 0f CSE <artofcse@gmail.com>
2019-12-03 20:44:23 +0600

    Update bst

--- Data-Structure/16-bst-while-loop.py
+++ Data-Structure/16-bst-while-loop.py
@@ -14,48 +14,53 @@ class Tree():
 	def __init__(self):
 		self.root = None
 
-
+	# Insert a node to the tree
 	def insert(self, data):
 		if self.root is None:
 			self.root = Node(data)
 			return
 
 		current_node = self.root
-		prev_node = {};
+		prev_node = None
 
 		while current_node:
-			prev_node['node'] = current_node
+			prev_node = current_node
 			if data < current_node.data:
-				prev_node['side'] = 'left'
 				current_node = current_node.left
 			else:
-				prev_node['side'] = 'right'
 				current_node = current_node.right
 		
-		if prev_node['side'] == 'left':
-			prev_node['node'].left = Node(data)
+		if data < prev_node.data:
+			prev_node.left = Node(data)
 		else:
-			prev_node['node'].right = Node(data)
+			prev_node.right = Node(data)
 			
 
-
+	# Build the tree
 	def build_tree(self):
-		self.insert(7)
-		self.insert(6)
-		self.insert(8)
-		self.insert(5)
-		self.insert(9)
-		self.insert(2)
-		self.insert(12)
-		self.insert(3)
+		for x in [7, 6, 8, 5, 9, 2, 12, 3]:
+			self.insert(x)
 
 
+	# Print the tree
 	def in_order(self, node):
 		if node.left:	
 			self.in_order(node.left)
 		print(node, end=' ')
 		if node.right:
 			self.in_order(node.right)
+
+	# Search in tree
+	def search_bst(self, node, key):
+		while node is not None:
+			if node.data == key:
+				return node
+			if key < node.data:
+				node = node.left
+			else:
+				node = node.right
+
+		return 'Not found'
 
 
 
@@ -64,4 +69,7 @@ if __name__ == '__main__':
 	my_tree.build_tree()
 
 	my_tree.in_order(my_tree.root)
+	print('\nSearch data')
+	print( my_tree.search_bst(my_tree.root, 2) )
+	print( my_tree.search_bst(my_tree.root, 15) )
 	print('')
